bb79fb57b5c198b9875f2a82921bfc8c446e640b
hyun6ik <fkanwm@naver.com>
2021-02-26 21:40:04 +0900

    회원 수정 API

--- Spring-FourtheenthProject/src/main/java/springeighthproject/spring_jpa/api/MemberApiController.java
+++ Spring-FourtheenthProject/src/main/java/springeighthproject/spring_jpa/api/MemberApiController.java
@@ -1,10 +1,9 @@
 package springeighthproject.spring_jpa.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springeighthproject.spring_jpa.domain.Member;
 import springeighthproject.spring_jpa.service.MemberService;
 
@@ -34,6 +33,26 @@ public class MemberApiController {
         Long id = memberService.join(member);
 
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberv2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request){
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
     }
 
     @Data
