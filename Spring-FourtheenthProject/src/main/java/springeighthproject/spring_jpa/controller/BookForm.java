05acbbb50a91ab046e072fa96154921f97e29c20
hyun6ik <fkanwm@naver.com>
2021-02-26 20:51:47 +0900

    API 개발과 성능 최적화

---
+++ Spring-FourtheenthProject/src/main/java/springeighthproject/spring_jpa/controller/BookForm.java
@@ -0,0 +1,15 @@
+package springeighthproject.spring_jpa.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private String name;
+    private Long id;
+    private int price;
+    private int stockQuantity;
+    private String author;
+    private String isbn;
+}
