899484631b69317793157fa7c568e5b23a1f2eec
hyun6ik <fkanwm@naver.com>
2021-02-28 22:37:27 +0900

    간단한 주문 조회 V3 : 엔티티를 DTO로 변환 - 페치조인 최적화

--- Spring-FourtheenthProject/src/main/java/springeighthproject/spring_jpa/api/OrderSimpleApiController.java
+++ Spring-FourtheenthProject/src/main/java/springeighthproject/spring_jpa/api/OrderSimpleApiController.java
@@ -44,8 +44,17 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v2/simple-orders")
     public List<SimpleOrderDto> ordersV2(){
         //Order 2개
-        //N + 1 -> 1 + 회원 N + 배송 N 
+        //N + 1 -> 1 + 회원 N + 배송 N
        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
         List<SimpleOrderDto> result = orders.stream()
                 .map(o -> new SimpleOrderDto(o))
                 .collect(Collectors.toList());
